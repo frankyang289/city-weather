afb68dbe32d8c15a7fa97d13f4cd8a78146ebd00
Frank Yang <frank.yang2@bp.com>
2026-02-26 17:10:23 +0000

    feat(bottom nav): bottom nav displaying with empty screen

--- app/src/main/java/com/heavywater/cityweather/MainActivity.kt
+++ app/src/main/java/com/heavywater/cityweather/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.heavywater.cityweather
 
+import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import com.heavywater.cityweather.ui.CityWeatherApp
 
 class MainActivity : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CityWeatherApp()
+        }
+    }
 }
--- app/src/main/java/com/heavywater/cityweather/navigation/BottomNavigationItem.kt
+++ app/src/main/java/com/heavywater/cityweather/navigation/BottomNavigationItem.kt
@@ -9,28 +9,26 @@ import com.heavywater.cityweather.feature.currentweather.api.R as currentWeather
 import com.heavywater.cityweather.feature.favourites.api.R as favouritesR
 
 
-class BottomNavigationItem {
-    data class BottomNavigationItem(
-        @StringRes val labelTextId: Int? = null,
-        val selectedIcon: ImageVector? = null,
-        val unselectedIcon: ImageVector? = null
-    )
+data class BottomNavigationItem(
+    @StringRes val labelTextId: Int? = null,
+    val selectedIcon: ImageVector? = null,
+    val unselectedIcon: ImageVector? = null
+)
 
-    val CURRENT_WEATHER = BottomNavigationItem(
-        currentWeatherR.string.feature_currentweather_api_title,
-        AppIcons.currentWeather,
-        AppIcons.currentWeatherBorder
+val CURRENT_WEATHER = BottomNavigationItem(
+    currentWeatherR.string.feature_currentweather_api_title,
+    AppIcons.currentWeather,
+    AppIcons.currentWeatherBorder
 
-    )
+)
 
-    val FAVOURITES = BottomNavigationItem(
-        favouritesR.string.feature_favourites_api_title,
-        AppIcons.favourites,
-        AppIcons.favouritesBorder
-    )
+val FAVOURITES = BottomNavigationItem(
+    favouritesR.string.feature_favourites_api_title,
+    AppIcons.favourites,
+    AppIcons.favouritesBorder
+)
 
-    val BOTTOM_NAV_ITEMS = mapOf(
-        CurrentWeatherNavKey to CURRENT_WEATHER,
-        FavouritesNavKey to FAVOURITES
-    )
-}
+val BOTTOM_NAV_ITEMS = mapOf(
+    CurrentWeatherNavKey to CURRENT_WEATHER,
+    FavouritesNavKey to FAVOURITES
+)
